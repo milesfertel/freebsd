09fd748496cb2457d5655d558083ec68509b821a
Miles Fertel <milesfertel@college.harvard.edu>
2017-06-18 15:52:14 +0200

    A more comprehensive set of tests for mergesort

--- lib/libc/tests/stdlib/mergesort_test.c
+++ lib/libc/tests/stdlib/mergesort_test.c
@@ -29,17 +29,119 @@
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
+__FBSDID("$FreeBSD: head/lib/libc/tests/stdlib/mergesort_test.c 290538 2015-11-08 07:03:17Z ngie $");
 
 #include <assert.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "test-sort.h"
+#define ELT_MAX 16
 
-ATF_TC_WITHOUT_HEAD(mergesort_test);
-ATF_TC_BODY(mergesort_test, tc)
-{
+struct int_array {
+	int len;
+	int elts[ELT_MAX];
+};
+
+struct char_array {
+	int len;
+	char elts[ELT_MAX];
+};
+
+ATF_TC_WITHOUT_HEAD(trivial_mergesort_test);
+ATF_TC_BODY(trivial_mergesort_test, tc) {
+	struct int_array testarrays[] = {
+	    {1, {0}},       {2, {0, 0}},    {3, {0, 0, 0}},   {2, {0, 1}},
+	    {2, {1, 0}},    {3, {0, 1, 2}}, {3, {0, 2, 1}},   {3, {1, 0, 2}},
+	    {3, {1, 2, 0}}, {3, {2, 0, 1}}, {3, {2, 1, 0}},   {3, {0, 1, 1}},
+	    {3, {1, 0, 1}}, {3, {1, 1, 0}}, {3, {0, -1, -1}}, {3, {0, -1, 0}}};
+
+	int sresvector[ELT_MAX];
+	int testvector[ELT_MAX];
+	int num_tests = nitems(testarrays);
+
+	for (int i = 0; i < num_tests; i++) {
+		int len = testarrays[i].len;
+		/* Populate test vectors */
+		for (int j = 0; j < len; j++)
+			testvector[j] = sresvector[j] = testarrays[i].elts[j];
+		/* Sort using mergesort(3) */
+		mergesort(testvector, len, sizeof(testvector[0]), sorthelp);
+		/* Sort using reference slow sorting routine */
+		ssort(sresvector, len);
+
+		for (int k = 0; k < len; k++)
+			ATF_CHECK_MSG(
+			    testvector[k] == sresvector[k],
+			    "item at index %d didn't match: %d != %d", k,
+			    testvector[k], sresvector[k]);
+	}
+}
+
+ATF_TC_WITHOUT_HEAD(ordering_mergesort_test);
+ATF_TC_BODY(ordering_mergesort_test, tc) {
+	struct int_array testarrays[] = {
+	    {3, {-1, -2, -3}},
+	    {3, {-3, -2, -1}},
+	    {11, {1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1}},
+	    {10, {5, 5, 4, 4, 3, 3, 2, 2, 1, 1}},
+	    {10, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+	    {10, {1, 2, 3, 4, 5, 6, -7, -8, -9, -10}},
+	    {10, {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}}};
+
+	int sresvector[ELT_MAX];
+	int testvector[ELT_MAX];
+	int num_tests = nitems(testarrays);
+
+	for (int i = 0; i < num_tests; i++) {
+		int len = testarrays[i].len;
+		/* Populate test vectors */
+		for (int j = 0; j < len; j++)
+			testvector[j] = sresvector[j] = testarrays[i].elts[j];
+		/* Sort using mergesort(3) */
+		mergesort(testvector, len, sizeof(testvector[0]), sorthelp);
+		/* Sort using reference slow sorting routine */
+		ssort(sresvector, len);
+
+		for (int k = 0; k < len; k++)
+			ATF_CHECK_MSG(
+			    testvector[k] == sresvector[k],
+			    "item at index %d didn't match: %d != %d", k,
+			    testvector[k], sresvector[k]);
+	}
+}
+
+ATF_TC_WITHOUT_HEAD(misc_mergesort_test);
+ATF_TC_BODY(misc_mergesort_test, tc) {
+	struct int_array testarrays[] = {
+	    {10, {42, 9, 17, 123, 602, -3, 123, 969, -11, INT_MAX}},
+	    {10, {-11, -3, 9, 17, 42, 54, 54, 602, 969, INT_MIN}}};
+
+	int sresvector[ELT_MAX];
+	int testvector[ELT_MAX];
+	int num_tests = nitems(testarrays);
+
+	for (int i = 0; i < num_tests; i++) {
+		int len = testarrays[i].len;
+		/* Populate test vectors */
+		for (int j = 0; j < len; j++)
+			testvector[j] = sresvector[j] = testarrays[i].elts[j];
+		/* Sort using mergesort(3) */
+		mergesort(testvector, len, sizeof(testvector[0]), sorthelp);
+		/* Sort using reference slow sorting routine */
+		ssort(sresvector, len);
+
+		for (int k = 0; k < len; k++)
+			ATF_CHECK_MSG(
+			    testvector[k] == sresvector[k],
+			    "item at index %d didn't match: %d != %d", k,
+			    testvector[k], sresvector[k]);
+	}
+}
+
+ATF_TC_WITHOUT_HEAD(rand_mergesort_test);
+ATF_TC_BODY(rand_mergesort_test, tc) {
 	int sresvector[IVEC_LEN];
 	int testvector[IVEC_LEN];
 	int i, j;
@@ -56,16 +158,75 @@ ATF_TC_BODY(mergesort_test, tc)
 
 		/* Compare results */
 		for (i = 0; i < j; i++)
-			ATF_CHECK_MSG(testvector[i] == sresvector[i],
-			    "item at index %d didn't match: %d != %d",
-			    i, testvector[i], sresvector[i]);
+			ATF_CHECK_MSG(
+			    testvector[i] == sresvector[i],
+			    "item at index %d didn't match: %d != %d", i,
+			    testvector[i], sresvector[i]);
 	}
 }
 
-ATF_TP_ADD_TCS(tp)
-{
+ATF_TC_WITHOUT_HEAD(small_elt_mergesort_test);
+ATF_TC_BODY(small_elt_mergesort_test, tc) {
+	struct char_array testarrays[] = {
+	    {1, {0}},
+	    {2, {0, 0}},
+	    {3, {0, 0, 0}},
+	    {2, {0, 1}},
+	    {2, {1, 0}},
+	    {3, {0, 1, 2}},
+	    {3, {0, 2, 1}},
+	    {3, {1, 0, 2}},
+	    {3, {1, 2, 0}},
+	    {3, {2, 0, 1}},
+	    {3, {2, 1, 0}},
+	    {3, {0, 1, 1}},
+	    {3, {1, 0, 1}},
+	    {3, {1, 1, 0}},
+	    {3, {0, -1, -1}},
+	    {3, {0, -1, 0}},
+	    {3, {-1, -2, -3}},
+	    {3, {-3, -2, -1}},
+	    {11, {1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1}},
+	    {10, {5, 5, 4, 4, 3, 3, 2, 2, 1, 1}},
+	    {10, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+	    {10, {1, 2, 3, 4, 5, 6, -7, -8, -9, -10}},
+	    {10, {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}},
+	    {10, {42, 9, 17, 123, 62, -3, 123, 99, -11, CHAR_MAX}},
+	    {10, {-11, -3, 9, 17, 42, 54, 54, -62, 9, CHAR_MIN}}};
 
-	ATF_TP_ADD_TC(tp, mergesort_test);
+	char sresvector[ELT_MAX];
+	char testvector[ELT_MAX];
+	int num_tests = nitems(testarrays);
+
+	for (int i = 0; i < num_tests; i++) {
+		int len = testarrays[i].len;
+		/* Populate test vectors */
+		for (int j = 0; j < len; j++)
+			testvector[j] = sresvector[j] = testarrays[i].elts[j];
+		/* Sort using mergesort(3) */
+		atf_tc_expect_fail(
+		    "Mergesort implementation does not support arguments less "
+		    "than sizeof(void *)/2");
+		ATF_CHECK(
+		    mergesort(
+			testvector, len, sizeof(testvector[0]), sorthelp) == 0);
+		/* Sort using reference slow sorting routine */
+		scsort(sresvector, len);
+
+		for (int k = 0; k < len; k++)
+			ATF_CHECK_MSG(testvector[k] == sresvector[k],
+			    "item at index %d didn't match: %d != %d", k,
+			    testvector[k], sresvector[k]);
+	}
+}
+
+ATF_TP_ADD_TCS(tp) {
+	ATF_TP_ADD_TC(tp, trivial_mergesort_test);
+	ATF_TP_ADD_TC(tp, ordering_mergesort_test);
+	ATF_TP_ADD_TC(tp, misc_mergesort_test);
+	ATF_TP_ADD_TC(tp, rand_mergesort_test);
+	/* This will fail until new mergesort is completed */
+	ATF_TP_ADD_TC(tp, small_elt_mergesort_test);
 
 	return (atf_no_error());
 }
